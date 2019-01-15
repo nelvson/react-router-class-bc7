3772b70b3e06d8981aa8fb114325137ea2bf6c9c
Dominggus Octovianus <dominggus.octovianus@kodefox.com>
2019-01-15 03:00:47 -0600

    Revert "Setup flow (#1)"
    This reverts commit a8843a8a50de478405cfb7e89c483dd6a5359374.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-// @flow
-
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component<{}> {
+class App extends Component {
   render() {
     return (
       <div className="App">
