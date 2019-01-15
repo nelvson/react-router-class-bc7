604f224a5ed6cece15b538d0dee68f736b99edc6
Filbert <39455112+FilbertAW@users.noreply.github.com>
2019-01-15 17:26:30 +0700

    add home page (#4)
    * add home page
    
    * minor fixed

---
+++ pages/home.js
@@ -0,0 +1,15 @@
+// @flow
+
+import React, {Component} from 'react';
+
+class App extends Component<{}> {
+  render() {
+    return (
+      <div className="App">
+            this is a home page.
+      </div>
+    );
+  }
+}
+
+export default App;
