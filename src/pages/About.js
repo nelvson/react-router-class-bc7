6656d2e5acf148bee316dcd90b997be7a4a7afb5
Alvin <ajk.alvin@gmail.com>
2019-01-15 16:57:01 +0700

    Dev About Page

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+//@flow
+import React from 'react';
+type Props = {};
+export default function About(props: Props) {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div>
+        <p>We are bootcamp 7</p>
+      </div>
+    </div>
+  );
+}
